a94d02b620cb04be821674cee168944478876ddb
Micael Martins de Moura <micael.mdemoura@gmail.com>
2025-09-24 15:49:15 -0300

    Commit Inicial

---
+++ CoreChallenge/CoreChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoreChallenge
+//
+//  Created by Micael Martins de Moura on 24/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CoreChallenge/CoreChallenge/CoreChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreChallengeApp.swift
+//  CoreChallenge
+//
+//  Created by Micael Martins de Moura on 24/09/25.
+//
+
+import SwiftUI
+
+@main
+struct CoreChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
