001afaef3e72c17b9fe0740766cc2955f6adeced
Pedro Santos <pedrohenriqueph004@gmail.com>
2025-09-30 17:27:42 -0300

    Creating the Sound Analysis functions

--- CoreChallenge/CoreChallenge/Photo/ViewModel/PhotoViewModel.swift
+++ CoreChallenge/CoreChallenge/Photo/ViewModel/PhotoViewModel.swift
@@ -24,6 +24,8 @@ class PhotoViewModel {
     /// Foto bruta selecionada pelo usuário através do `PhotosPicker`.
     var selectedPhoto: PhotosPickerItem?
     
+    var imageIsSelected: Bool = false
+    
     /// Indicador se o animal detectado é doméstico (`true`) ou não (`false`).
     var domestic: Bool = true
     
@@ -36,6 +38,8 @@ class PhotoViewModel {
     /// - Se a conversão for bem-sucedida, atualiza a propriedade `image`
     ///   com uma animação suave.
     func convertDataToImage() async {
+        
+        print("cheguei boy")
         if let selectedPhoto,
            let data = try? await selectedPhoto.loadTransferable(type: Data.self) {
             if let uiimage = UIImage(data: data) {
